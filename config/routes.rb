cda913900240604f95688594a1c638f80bb98414
Metamorph Dev <42251514+gtonussi@users.noreply.github.com>
2019-05-04 20:46:42 -0300

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+
+  resources :produtos, only: [:new, :create, :destroy]
+
+  root to: "produtos#index"
+
+
+end
